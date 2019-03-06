91940badf61c5c0a19929779239467245059df2b
Bill G <gbilly3@gmail.com>
2019-03-06 16:22:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 def reformat_languages(languages)
   new_hash = {}
   languages.collect do |style, list| 
   lang.collect do |lang, type|
     new_hash[lang] = type
+    binding.pry
     new_hash[lang][:style] = []
     
   
