85e925be421a2a0e88474bec283ea0537e881f65
Bill G <gbilly3@gmail.com>
2019-03-06 16:25:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,17 +1,18 @@
 require 'pry'
-
 def reformat_languages(languages)
+  # your code here
   new_hash = {}
-  languages.collect do |style, list| 
-  lang.collect do |lang, type|
-    new_hash[lang] = type
-    binding.pry
-    new_hash[lang][:style] = []
-    new_hash[lang][:style] << style
-  
-end 
-end 
 
+  languages.collect do |style, list|
+    list.collect do |lang, type|
+      # adding the type for each lang 
+      new_hash[lang] = type
+      # making the style for each lang into an array
+      new_hash[lang][:style] = []
+      # adding the style for each lang into said array
+      new_hash[lang][:style] << style
+    end
+  end
   
   new_hash
 end
