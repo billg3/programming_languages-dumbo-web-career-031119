2bcf40e43b7e126ea2f3bc2aefb157ed79408978
Bill G <gbilly3@gmail.com>
2019-03-06 15:21:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.collect do |style, list| 
+  lang.collect do |lang, type|
+    new_hash[lang] = type
+    
+  
+  
+  
+  
 end
+  new_hash
+end
+
+
+ :ruby => {
+    :type => "interpreted",
+    :style => [:oo]
