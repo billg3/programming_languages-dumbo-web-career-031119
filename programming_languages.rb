c5682f1845cd1e590e0a40942f40144c8f1572e7
Bill G <gbilly3@gmail.com>
2019-03-06 16:30:22 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,7 +8,7 @@ def reformat_languages(languages)
       # adding the type for each lang 
       new_hash[lang] = type
       # making the style for each lang into an array
-      new_hash[lang][:style] = style
+      new_hash[lang][:style] = []
       # adding the style for each lang into said array
      ## new_hash[lang][:style] << style
     end
